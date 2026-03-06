a4a45e156aa20aeccf54b4b7aca9c9cde3476ee7
Everolfe <rusakgeorgy@gmail.com>
2026-03-06 17:24:22 +0300

    Fix Specifications

--- src/main/java/com/github/everolfe/userservice/dao/UserSpecification.java
+++ src/main/java/com/github/everolfe/userservice/dao/UserSpecification.java
@@ -1,9 +1,13 @@
 package com.github.everolfe.userservice.dao;
 
 import com.github.everolfe.userservice.entity.User;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.data.jpa.domain.Specification;
 import jakarta.persistence.criteria.Predicate;
-import java.util.ArrayList;
+import java.util.Optional;
 import java.util.List;
 
 public class UserSpecification {
@@ -12,21 +16,12 @@ public class UserSpecification {
 
     public static Specification<User> filterByNameAndSurname(String name, String surname) {
         return (root, query, criteriaBuilder) -> {
-            List<Predicate> predicates = new ArrayList<>();
-
-            if (name != null && !name.trim().isEmpty()) {
-                predicates.add(criteriaBuilder.like(
-                        criteriaBuilder.lower(root.get("name")),
-                        "%" + name.toLowerCase() + "%"
-                ));
-            }
-
-            if (surname != null && !surname.trim().isEmpty()) {
-                predicates.add(criteriaBuilder.like(
-                        criteriaBuilder.lower(root.get("surname")),
-                        "%" + surname.toLowerCase() + "%"
-                ));
-            }
+            List<Predicate> predicates = Stream.of(
+                            createLikePredicate(criteriaBuilder, root.get("name"), name),
+                            createLikePredicate(criteriaBuilder, root.get("surname"), surname)
+                    ).filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toList());
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
@@ -58,5 +53,18 @@ public class UserSpecification {
                 prefix == null ? criteriaBuilder.conjunction() :
                         criteriaBuilder.like(criteriaBuilder
                                 .lower(root.get("surname")), prefix.toLowerCase() + "%");
+    }
+
+    private static Optional<Predicate> createLikePredicate(
+            CriteriaBuilder criteriaBuilder,
+            Path<String> field,
+            String value) {
+
+        return Optional.ofNullable(value)
+                .filter(v -> !v.trim().isEmpty())
+                .map(v -> criteriaBuilder.like(
+                        criteriaBuilder.lower(field),
+                        "%" + v.toLowerCase() + "%"
+                ));
     }
 }
