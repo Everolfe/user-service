f501baee3004510cc316df5f67927a0027888e17
Everolfe <rusakgeorgy@gmail.com>
2026-03-07 16:22:50 +0300

    Update PaymentCardRepository

--- src/main/java/com/github/everolfe/userservice/dao/PaymentCardRepository.java
+++ src/main/java/com/github/everolfe/userservice/dao/PaymentCardRepository.java
@@ -1,7 +1,7 @@
 package com.github.everolfe.userservice.dao;
 
 import com.github.everolfe.userservice.entity.PaymentCard;
-import jakarta.transaction.Transactional;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface PaymentCartRepository extends JpaRepository<PaymentCard, Long>,
+public interface PaymentCardRepository extends JpaRepository<PaymentCard, Long>,
         JpaSpecificationExecutor<PaymentCard> {
 
     @Modifying
@@ -30,10 +30,8 @@ public interface PaymentCartRepository extends JpaRepository<PaymentCard, Long>,
             "WHERE pc.id = :#{#card.id}")
     int updateCardDynamic(@Param("card") PaymentCard card);
 
-    @Query(value = "SELECT * FROM payment_cards WHERE user_id = :userId",
-            countQuery = "SELECT COUNT(*) FROM payment_cards WHERE user_id = :userId",
-            nativeQuery = true)
-    Page<PaymentCard> findAllCardsByUserIdNative(@Param("userId") Long userId, Pageable pageable);
+    @Query("SELECT pc FROM PaymentCard pc WHERE pc.user.id = :userId")
+    List<PaymentCard> findAllCardsByUserId(@Param("userId") Long userId);
 
     @Query("SELECT pc FROM PaymentCard pc WHERE pc.user.name LIKE %:name% AND pc.user.surname LIKE %:surname%")
     Page<PaymentCard> findCardsByUserNameAndSurname(@Param("name") String name,
