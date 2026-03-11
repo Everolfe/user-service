05310030786c9949ef79762572eb10f47db99536
Everolfe <rusakgeorgy@gmail.com>
2026-03-11 19:52:27 +0300

    Update PaymentCardService

--- src/main/java/com/github/everolfe/userservice/service/PaymentCardService.java
+++ src/main/java/com/github/everolfe/userservice/service/PaymentCardService.java
@@ -36,6 +36,12 @@ public class PaymentCardService {
     @Transactional
     public GetPaymentCardDto create(CreatePaymentCardDto createPaymentCardDto, Long userId) {
         PaymentCard paymentCard = createPaymentCardMapper.toEntity(createPaymentCardDto);
+        if (createPaymentCardDto.getNumber() != null &&
+                !createPaymentCardDto.getNumber().equals(paymentCard.getNumber()) &&
+                paymentCardRepository.existsByNumber(createPaymentCardDto.getNumber())) {
+            throw new DuplicateResourceException(
+                    "Card with number " + createPaymentCardDto.getNumber() + " already exists");
+        }
         Optional<User> user = userRepository.findById(userId);
         paymentCard.setUser(user
                 .orElseThrow(() -> new ResourceNotFoundException("No User with id" + userId)));
