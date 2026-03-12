2717d2720ca27e589f9e38ce20582c7973afadbc
Everolfe <rusakgeorgy@gmail.com>
2026-03-12 12:28:56 +0300

    20250312: create integration tests

---
+++ src/test/java/com/github/everolfe/userservice/integration/BaseIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.github.everolfe.userservice.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public class BaseIntegrationTest {
+
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:15-alpine")
+    )
+            .withReuse(true);;
+
+    @Container
+    static final GenericContainer<?> redis = new GenericContainer<>(
+            DockerImageName.parse("redis:7.2-alpine")
+    )
+            .withExposedPorts(6379)
+            .withReuse(true);;
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("DB_URL", postgres::getJdbcUrl);
+        registry.add("DB_USERNAME", postgres::getUsername);
+        registry.add("DB_PASSWORD", postgres::getPassword);
+        registry.add("DB_DRIVER", () -> "org.postgresql.Driver");
+        registry.add("DB_DIALECT", () -> "org.hibernate.dialect.PostgreSQLDialect");
+
+        registry.add("REDIS_HOST", redis::getHost);
+        registry.add("REDIS_PORT", () -> redis.getMappedPort(6379).toString());
+
+        registry.add("SHOW_SQL", () -> "true");
+        registry.add("LIQUIBASE_ENABLE", () -> "false");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.cache.type", () -> "redis");
+    }
+}
