f07a10d52322c381fbda67240965f0e6a4f7c8d3
Everolfe <rusakgeorgy@gmail.com>
2026-03-25 11:12:00 +0300

    250325: updates for auth service

---
+++ src/test/java/com/github/everolfe/userservice/integration/config/TestSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.github.everolfe.userservice.integration.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.mockito.Mockito.mock;
+
+@TestConfiguration
+@Profile("test")
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return mock(JwtDecoder.class);
+    }
+}
